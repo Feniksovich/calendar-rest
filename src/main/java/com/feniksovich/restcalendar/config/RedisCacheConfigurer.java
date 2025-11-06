ce79b0c84d2c10b5783e97ad4c2cba186bdff071
feniksovich <maxsimus.i@yandex.ru>
2025-11-06 16:20:37 +0600

    refactor: define dto serializer instead of generic

--- src/main/java/com/feniksovich/restcalendar/config/RedisCacheConfigurer.java
+++ src/main/java/com/feniksovich/restcalendar/config/RedisCacheConfigurer.java
@@ -1,10 +1,12 @@
 package com.feniksovich.restcalendar.config;
 
+import com.feniksovich.restcalendar.dto.CalendarDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -15,9 +17,12 @@ public class RedisCacheConfigurer {
      */
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
+        final RedisSerializer<CalendarDto> calendarDtoSerializer =
+                new Jackson2JsonRedisSerializer<>(CalendarDto.class);
+
         return RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(SerializationPair.fromSerializer(calendarDtoSerializer));
     }
 }
 
